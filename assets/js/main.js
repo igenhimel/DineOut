34cc4660d8aa6282e6943d63211043a867b4705a
igenhimel <djstar.himel@gmail.com>
2021-04-14 23:57:45 +0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,29 @@ $('.lunch').click(function(){
 
 })
 
+$('.img-hover').mouseenter(function () { 
+
+    $('img').addClass("imghov")
+    $('.col-12').children('.team-descrip').addClass("hovers")
+    $('.team-descrip').children().addClass("addColor")
+    $(this).parent().siblings().children('.team-descrip').removeClass("hovers")
+    $(this).parent().siblings().children().children('img').removeClass("imghov")
+    $(this).parent().siblings().children('.team-descrip').children().removeClass("addColor")
+   
+    
+    
+});
+
+$('.img-hover').mouseleave(function () { 
+    $('img').removeClass("imghov")
+
+    $('.col-12').children('.team-descrip').removeClass("hovers")
+    $('.team-descrip').children().removeClass("addColor")
+    
+    
+});
+
+
         $(window).scroll(function(){
          var y = $(window).scrollTop();
          
@@ -67,16 +90,48 @@ $('.lunch').click(function(){
    $('.lunch').click(function () { 
       
     $('#lunch').show('slow')
-    $('#dinner').hide('slow')
+    $('#dinner,#drinks,#desserts').hide('slow')
+
+   
  
        
    });
    $('.dinner').click(function () { 
       
     $('#dinner').show('slow')
-    $('#lunch').hide('slow')
+    $('#lunch,#drinks,#desserts').hide('slow')
+
+    
        
    });
+   $('.drinks').click(function () { 
+      
+    $('#drinks').show('slow')
+    $('#dinner,#lunch,#desserts').hide('slow')
+
+   
+ 
+       
+   });
+   $('.desserts').click(function () { 
+      
+    $('#desserts').show('slow')
+    $('#lunch,#dinner,#drinks').hide('slow')
+
+    
+       
+   });
+
+
+   $(".mixbtn").click(function () { 
+
+    $(this).addClass('mix-active')
+    $(this).siblings().removeClass('mix-active')
+    $('.mixbtn').removeClass('active')
+       
+       
+   });
+
 
 });
 
